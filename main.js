bd91017aa3e536cf737dbc5ee2ff6e32cab04013
Elsokkary101 <68373114+Elsokkary101@users.noreply.github.com>
2022-02-27 00:52:14 +0200

    main functionality

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("masr");
+try {
+    const allmain = document.getElementsByClassName("card-body");
+    for (var i = 0;  i  < allmain.length ; i++) {
+        var str = allmain[i].children[0].children[0].innerHTML;
+        allmain[i].children[2].children[0].children[0].download = str; 
+    }
+  }
+  catch(err) {
+    console.log("ta7ya masr");
+  }
